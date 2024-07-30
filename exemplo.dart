fb86389c2d399e8b61b95385d6fb0dbe2f0e9506
Paulo Vanderley <vanderley.1109@gmail.com>
2024-07-30 15:35:17 -0300

    :page_with_curl: Update file content.

---
+++ exemplo.dart
@@ -0,0 +1,18 @@
+// Definindo a superclasse Veiculo.
+class Veiculo {
+  String marca;
+  int ano;
+
+  Veiculo(
+    this.marca,
+    this.ano,
+  );
+
+  void mostrarDetalhe() {
+    print('Marca: $marca, Ano: $ano');
+  }
+}
+
+// Definindo a subclasse Carro.
+
+// Definindo a subclasse Moto.
